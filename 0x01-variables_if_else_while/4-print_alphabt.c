752d081daa4355bee3812ddc519c4bf6b3920a03
BigRacle12 <ezemiracle13@gmail.com>
2022-09-09 17:39:56 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 /**
- *
  * main - main block
- *
  * Description: Use `putchar` to print all letters but the letter 'q' and 'e'.
- *
  * Return: 0
- *
  */
 
-int main(void)
+int main
 
 {
 	char c = 'a';
