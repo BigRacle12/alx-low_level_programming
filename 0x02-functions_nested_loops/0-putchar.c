40944777c8b3a1ed39de2a564263869fdfdb48c1
BigRacle12 <ezemiracle13@gmail.com>
2022-09-13 17:25:48 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * main - Print "Holberton" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
-
 int main(void)
 {
 	char putchar[] = "_putchar";
@@ -17,6 +15,5 @@ int main(void)
 		i++;
 	}
 	_putchar('\n');
-	
 	return (0);
 }
